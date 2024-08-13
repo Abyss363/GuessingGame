ea8b2dd2b6724b6aff2791ccd904bf0a5663d854
Abyss363 <fortuneodo@gmail.com>
2024-08-13 12:03:58 +0100

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string guessWord = "animal";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            do
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else { outOfGuesses = true; }
+            }
+            while(guess != guessWord && !outOfGuesses);
+
+            if (outOfGuesses )
+            {
+                Console.WriteLine("You Lose!!");
+            }
+            else { Console.WriteLine("You Win!!"); }
+
+        }
+    }
+}
